5c73407cafa87642d49ee5e5db4695947e92753c
skrillberg <brian.kilberg@gmail.com>
2017-10-25 15:26:04 -0700

    serial port code working

--- Mimsy2-Apps/applications/leds_example.c
+++ Mimsy2-Apps/applications/leds_example.c
@@ -67,6 +67,9 @@
 #include "usb_cdc.h"
 #include "usb_in_buffer.h"
 #include "usb_out_buffer.h"
+#include "uart.h"
+#include "hw_ioc.h"
+
 
 
 /******************************************************************************
@@ -79,6 +82,10 @@
 #define PAGE_SIZE                2048
 #define PAGE_TO_ERASE            14
 #define PAGE_TO_ERASE_START_ADDR (FLASH_BASE + (PAGE_TO_ERASE * PAGE_SIZE))
+
+#define EXAMPLE_PIN_UART_RXD            GPIO_PIN_2 
+#define EXAMPLE_PIN_UART_TXD            GPIO_PIN_1 
+#define EXAMPLE_GPIO_BASE               GPIO_D_BASE
 
 /******************************************************************************
 
@@ -178,7 +185,15 @@ Timer1BIntHandler(void)
 void main(void)
 {
     volatile uint32_t ui32Loop;
-   // SysCtrlClockSet(false,false,SYS_CTRL_SYSDIV_32MHZ);
+    SysCtrlClockSet(false,false,SYS_CTRL_SYSDIV_32MHZ);
+    
+        //
+    // Set IO clock to the same as system clock
+    //
+    SysCtrlIOClockSet(SYS_CTRL_SYSDIV_32MHZ);
+   
+    
+    
     //
     // Init LEDs (turned off)
     //
@@ -344,7 +359,63 @@ i2c_read_byte(address,byteptr);
      
          
 
+////////////////////////////////////////////////////////////////uart
+       char cThisChar;
 
+    //
+    // Set the clocking to run directly from the external crystal/oscillator.
+    // (no ext 32k osc, no internal osc)
+    //
+   // SysCtrlClockSet(false, false, SYS_CTRL_SYSDIV_32MHZ);
+
+
+    //
+    // Enable UART peripheral module
+    //
+    SysCtrlPeripheralEnable(SYS_CTRL_PERIPH_UART0);
+
+    //
+    // Disable UART function
+    //
+    UARTDisable(UART0_BASE);
+
+    //
+    // Disable all UART module interrupts
+    //
+    UARTIntDisable(UART0_BASE, 0x1FFF);
+
+    //
+    // Set IO clock as UART clock source
+    //
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Map UART signals to the correct GPIO pins and configure them as
+    // hardware controlled.
+    //
+    IOCPinConfigPeriphOutput(EXAMPLE_GPIO_BASE, EXAMPLE_PIN_UART_TXD, IOC_MUX_OUT_SEL_UART0_TXD);
+    GPIOPinTypeUARTOutput(EXAMPLE_GPIO_BASE, EXAMPLE_PIN_UART_TXD); 
+    IOCPinConfigPeriphInput(EXAMPLE_GPIO_BASE, EXAMPLE_PIN_UART_RXD, IOC_UARTRXD_UART0);
+    GPIOPinTypeUARTInput(EXAMPLE_GPIO_BASE, EXAMPLE_PIN_UART_RXD);
+     
+    //
+    // Configure the UART for 115,200, 8-N-1 operation.
+    // This function uses SysCtrlClockGet() to get the system clock
+    // frequency.  This could be also be a variable or hard coded value
+    // instead of a function call.
+    //
+    UARTConfigSetExpClk(UART0_BASE, SysCtrlClockGet(), 115200,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                         UART_CONFIG_PAR_NONE));
+    UARTEnable(UART0_BASE);
+    
+    //
+    // Put a character to show start of example.  This will display on the
+    // terminal.
+    //
+    UARTCharPut(UART0_BASE, '!');
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     
    
